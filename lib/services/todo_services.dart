ebe6bf1ebffac9a4cc980a33d618377c3b323e7d
FlutLab.io <>
2022-01-03 12:48:23 +0000

    Autosave

--- lib/services/todo_services.dart
+++ lib/services/todo_services.dart
@@ -5,7 +5,15 @@ class TodoService {
 
   //get all todos
   Future<Response> getAllTodosRequest() async {
-    return await get(Uri.parse('$baseUrl/todos'));
+    return await get(
+      Uri.parse('$baseUrl/todos'),
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+        "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+    );
   }
 
   //create a todo

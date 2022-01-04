574cd2c736bf419bde1d6731a4e538fdd42ad46f
FlutLab.io <>
2022-01-04 10:29:02 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() //entry point of ur code, where the app starts from
 {
   runApp(
     DevicePreview(
-      enabled: true,
+      isEnabled: !kReleaseMode,
       builder: (context) => MyApp(),
     ),
   );

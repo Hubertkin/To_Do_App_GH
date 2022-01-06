fcf610d2f80d630c3cf8ce14c5429dec12bbd8dc
FlutLab.io <>
2022-01-06 07:23:02 +0000

    Autosave

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -282,7 +282,7 @@ class _TodoListViewWidgetState extends State<TodoListViewWidget> {
                     if (isUpdated) {
                       widget.selectedItem == 'todo' ? widget._unCompletedData.removeAt(index) : widget._CompletedData.removeAt(index);
                       setState(() {});
-                      widget.load!();
+
                       Fluttertoast.showToast(msg: "Todo marked as completed!", toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM, timeInSecForIosWeb: 2, backgroundColor: Colors.green, textColor: Colors.white, fontSize: 16.0);
                     } else {
                       Fluttertoast.showToast(msg: "Could not  mark todo as completed!", toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM, timeInSecForIosWeb: 2, backgroundColor: Colors.red, textColor: Colors.white, fontSize: 16.0);

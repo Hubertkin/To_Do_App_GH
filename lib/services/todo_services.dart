8c028c5cb89285ba8de69dc7e615c9763f904b82
FlutLab.io <>
2022-01-06 09:33:29 +0000

    Autosave

--- lib/services/todo_services.dart
+++ lib/services/todo_services.dart
@@ -21,14 +21,12 @@ class TodoService {
       description: description,
       deadline: deadline,
     );
-    String jsonUser = jsonEncode(usersTodo);
-    print(jsonUser);
     Map<String, dynamic> body = {
       'title': title,
       'description': description,
       'deadline': deadline.toString(),
     };
-    return await post(Uri.parse('$baseUrl/todos'), body: jsonUser);
+    return await post(Uri.parse('$baseUrl/todos'), body: json.encode(usersTodo));
   }
 
   ///get todo by id(one todo)
